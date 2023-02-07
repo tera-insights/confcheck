05f5b2b402ce411699e978d79319c17f167b9571
Siddhant Jain <jainsid123456789@gmail.com>
2023-02-07 20:32:47 +0000

    minor main class fix

--- main.go
+++ main.go
@@ -8,6 +8,15 @@ import (
 	configuration "github.com/tera-insights/go-akka-configuration"
 )
 
+// Node Struct for Hocon
+type Node struct {
+	Key    string
+	Value  interface{}
+	Parent *Node
+	Child  []*Node
+}
+
+// Map Struct for TOML
 type ConfigTree map[string]interface{}
 
 func main() {
@@ -31,4 +40,37 @@ func main() {
 	// Struct Compare for Both Trees
 
 	// Value Validation
+}
+
+// Building Hocon Config Tree
+func (n *Node) addChild(node *Node) {
+	n.Child = append(n.Child, node)
+	node.Parent = n
+}
+
+func buildConfTree(key string, value interface{}, parent *Node) *Node {
+	node := &Node{Key: key, Value: value}
+	if parent != nil {
+		parent.addChild(node)
+	}
+
+	if v, ok := value.(map[string]interface{}); ok {
+		for k, val := range v {
+			buildConfTree(k, val, node)
+		}
+	}
+	return node
+}
+
+// Print Config Tree - Test
+func printConfigTree(tree ConfigTree, indent string) {
+	for key, value := range tree {
+		switch v := value.(type) {
+		case map[string]interface{}:
+			fmt.Printf("%s%s:\n", indent, key)
+			printConfigTree(v, indent+"  ")
+		default:
+			fmt.Printf("%s%s = %v\n", indent, key, v)
+		}
+	}
 }
