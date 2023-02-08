4ddca2dff667299a23687d282425acf15de0d0d7
Siddhant Jain <Siddhant@host-10-247-64-146.dhcp.cns.ufl.edu>
2023-02-08 11:17:22 -0500

    temp

--- go.mod
+++ go.mod
@@ -2,6 +2,6 @@ module main
 
 go 1.17
 
-require github.com/tera-insights/go-akka-configuration v1.0.0
+require github.com/tera-insights/go-akka-configuration v2.0.0
 
 require github.com/BurntSushi/toml v1.2.1
---
+++ map.go
@@ -0,0 +1,23 @@
+package main
+
+type Position struct {
+	Line int
+	Col int
+	Len int
+}
+
+// map of [path - line - col]
+
+func getPostion
+
+
+func createMap() {
+
+	//get position from parser
+
+	//curr node from tree
+
+	//map entry [ "key" : {line: .. , col: ..} ]
+
+	
+}
