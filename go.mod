d615d13b5ddea35d0bd95b98072fe309c4ecfca9
Siddhant Jain <jainsid123456789@gmail.com>
2023-02-23 17:38:21 -0500

    pending changes

--- go.mod
+++ go.mod
@@ -3,5 +3,3 @@ module main
 go 1.19
 
 require github.com/tera-insights/go-akka-configuration v1.1.0
-
-require github.com/gurkankaymak/hocon v1.2.15
--- main.go
+++ main.go
@@ -1,118 +1,54 @@
 package main
 
-// import (
-// 	"fmt"
-// 	"os"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
 
-// 	"github.com/BurntSushi/toml"
-// 	//configuration "github.com/tera-insights/go-akka-configuration"
-// )
+	configuration "github.com/tera-insights/go-akka-configuration"
+)
 
-// // Node Struct for Hocon
-// type Node struct {
-// 	Key         string
-// 	Value       interface{}
-// 	Parent      *Node
-// 	Child       []*Node
-// 	Required    bool
-// 	//Position    *Position
-// 	Description string
-// 	Type        string
-// 	Error       []string
-// 	Warning     []string
-// 	Verbose     []string
-// }
+func main() {
+	hfile := "files/config.conf"
+	hconf := configuration.LoadConfig(hfile)
 
-// // Map Struct for TOML
-// type ConfigTree map[string]interface{}
+	file, err := os.Open(hfile)
+	if err != nil {
+		fmt.Println("Error opening file:", err)
+		return
+	}
+	defer file.Close()
 
-// func main1() {
+	scanner := bufio.NewScanner(file)
+	line := 1
+	for scanner.Scan() {
+		text := scanner.Text()
+		if i := strings.Index(text, "="); i != -1 {
+			key := strings.TrimRight(strings.TrimLeft(text[:i], " \t\n\r#"), " ")
+			//fmt.Println("key:", key)
+			value := hconf.GetString(key)
+			if err == nil {
+				fmt.Printf("Key '%s' has value '%s' at line %d, column %d\n", key, value, line, i+1)
+			} else {
+				fmt.Println("Error retrieving value for key:", err)
+			}
+		}
+		line++
+	}
 
-// 	// Parsing TOML Config File into a Tree DS
-// 	var config ConfigTree
-// 	specFile := "/workspaces/confcheck/files/spec.toml"
-// 	_, err := toml.DecodeFile(specFile, &config)
-// 	if err != nil {
-// 		fmt.Fprintln(os.Stderr, "Error decoding toml file:", err)
-// 		os.Exit(1)
-// 	}
-// 	//PrintConfigTree(config, "")
+	if err := scanner.Err(); err != nil {
+		fmt.Println("Error reading file:", err)
+		return
+	}
+}
 
-// 	// Parsing HOCON Config File into a Tree DS
-// 	//hoconFile := "/workspaces/confcheck/files/config.conf"
-// 	// hoconConf := configuration.LoadConfig(hoconFile)
+func addKeyPosition(keyPositions map[string][2]int, key string, line int, col int) {
+	keyPositions[key] = [2]int{line, col}
+}
 
-// 	// tempMap := map[string][2]int{}
-// 	//BuildConfTree("hoconFile", hoconConf, nil , tempMap)
-// 	//fmt.Println(root)
-
-
-
-// 	// Creating a Map of Keys and Location (line / col)
-// 	//(testing for toml tree)
-// 	// keyMap := map[string]Node{}
-// 	// tempMap := map[string][2]int{}
-// 	// Dfs(config, hoconConf, "", nil, keyMap, tempMap)
-// 	// PrintMapValues(tempMap)
-
-// }
-
-// // Building Hocon Config Tree
-// func (n *Node) addChild(node *Node) {
-// 	n.Child = append(n.Child, node)
-// 	node.Parent = n
-// }
-
-// // func buildTree(key string, value interface{}, parent *Node, config *akka.Config) *Node {
-// // 	node := &Node{
-// // 		Key:    key,
-// // 		Value:  value,
-// // 		Parent: parent,
-// // 		Child:  []*Node{},
-// // 	}
-
-// // 	if parent != nil {
-// // 		parent.addChild(node)
-// // 	}
-
-// // 	loc, _ := config.GetPosition(key)
-// // 	if loc != hocon.Position{} {
-// // 		node.Data = [2]int{loc.Line, loc.Column}
-// // 	}
-
-// // 	if v, ok := value.(map[string]interface{}); ok {
-// // 		for k, val := range v {
-// // 			buildTree(k, val, node, config)
-// // 		}
-// // 	}
-
-// // 	return node
-// // }
-
-
-
-// // Print Config Tree - Test
-// func PrintConfigTree(tree ConfigTree, indent string) {
-// 	for key, value := range tree {
-// 		switch v := value.(type) {
-// 		case map[string]interface{}:
-// 			fmt.Printf("%s%s:\n", indent, key)
-// 			PrintConfigTree(v, indent+"  ")
-// 		default:
-// 			fmt.Printf("%s%s = %v\n", indent, key, v)
-// 		}
-// 	}
-// }
-
-// //Add values to Map
-// func AddToMap(mapp map[string][2]int, key string, value [2]int) {
-//     mapp[key] = value
-// }
-
-// //Print values to Map
-// func PrintMapValues(mapp map[string][2]int) {
-//     for _, value := range mapp {
-//         fmt.Println(value)
-//     }
-// 	fmt.Println("Printed")
-// }
+func printKeyPositions(keyPositions map[string][2]int) {
+	for key, pos := range keyPositions {
+		fmt.Printf("Key '%s' found at line %d, column %d\n", key, pos[0], pos[1])
+	}
+}
