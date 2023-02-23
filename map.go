1092fd123088cf1bd15929d67678aba24730ef9a
Siddhant Jain <jainsid123456789@gmail.com>
2023-02-23 17:43:26 -0500

    map/position class added

--- map.go
+++ map.go
@@ -1,63 +1,62 @@
 package main
 
-// import (
-// 	"fmt"
-// 	"strings"
+import (
+	"fmt"
+	"strings"
 
-// 	configuration "github.com/tera-insights/go-akka-configuration"
-// 	"github.com/tera-insights/go-akka-configuration/hocon"
-// 	"github.com/mitchellh/mapstructure"
-// )
-// type Position struct {
-// 	Line int
-// 	Col int
-// 	Len int
-// }
+	"github.com/mitchellh/mapstructure"
+	configuration "github.com/tera-insights/go-akka-configuration"
+	"github.com/tera-insights/go-akka-configuration/hocon"
+)
 
-// func NewPosition (pos interface{}) *Position {
-// 	res := Position{}
-// 	switch t := pos.(type) {
-// 	case hocon.Position:
-// 		x := pos.(hocon.Position)
-// 		res.Line = x.Line
-// 		res.Col = x.Col
-// 		res.Len = x.Len
-// 	default:
-// 		fmt.Printf("type %v is not supported \n", t)
-// 	}
-// 	return &res
-// }
+type Position struct {
+	Line int
+	Col  int
+	Len  int
+}
 
+func NewPosition(pos interface{}) *Position {
+	res := Position{}
+	switch t := pos.(type) {
+	case hocon.Position:
+		x := pos.(hocon.Position)
+		res.Line = x.Line
+		res.Col = x.Col
+		res.Len = x.Len
+	default:
+		fmt.Printf("type %v is not supported \n", t)
+	}
+	return &res
+}
 
+func Dfs(tree ConfigTree, config *configuration.Config, key string, parentNode ConfigTree, visited map[string]Node, tempMap map[string][2]int) {
+	for i, j := range tree {
+		_ = j
+		nestedtree, ok := tree[i].(map[string]interface{})
+		if ok {
+			c := key
+			if len(key) > 0 {
+				c = key + "."
+			}
+			Dfs(nestedtree, config, c+i, tree, visited, tempMap)
+		} else {
+			currNode, ok := visited[key]
+			if !ok {
+				currNode = Node{}
+				currMap := parentNode[key[1+strings.LastIndex(key, "."):]]
+				mapstructure.Decode(currMap, &currNode)
+				currNode.Key = key
+				//currNode.Position = NewPosition(config.GetPosition(key))
 
-// func Dfs(tree ConfigTree, config *configuration.Config, key string, parentNode ConfigTree, visited map[string]Node , tempMap map[string][2]int) {
-// 	for i, j := range tree {
-// 		_ = j
-// 		nestedtree, ok := tree[i].(map[string]interface{})
-// 		if ok {
-// 			c := key
-// 			if len(key) > 0 {
-// 				c = key + "."
-// 			}
-// 			Dfs(nestedtree, config, c+i, tree, visited, tempMap)
-// 		} else {
-// 			currNode, ok := visited[key]
-// 			if !ok {
-// 				currNode = Node{}
-// 				currMap := parentNode[key[1+strings.LastIndex(key, "."):]]
-// 				mapstructure.Decode(currMap, &currNode)
-// 				currNode.Key = key
-// 				//currNode.Position = NewPosition(config.GetPosition(key))
-				
-// 				//add in map
-// 				AddToMap(tempMap, key, [2]int{config.Root().GetPosition().Line, config.Root().GetPosition().Col})
+				//add in map
+				AddToMap(tempMap, key, [2]int{config.Root().GetPosition().Line, config.Root().GetPosition().Col})
 
-// 				//validate
-// 				//validate(config, &currNode)
+				//validate
+				//validate(config, &currNode)
 
-// 				visited[key] = currNode
-// 				fmt.Printf("%v %v \n", config.GetPosition(key), key)
-// 			}
-// 		}
-// 	}
-// }
+				visited[key] = currNode
+				fmt.Printf("%v %v \n", config.GetPosition(key), key)
+			}
+		}
+	}
+}
--- position.go
+++ position.go
@@ -1,49 +1,49 @@
 package main
 
-// import (
-// 	"fmt"
+import (
+	"fmt"
 
-// 	"github.com/tera-insights/go-akka-configuration"
-// )
+	"github.com/tera-insights/go-akka-configuration"
+)
 
-// type node struct {
-// 	NodeName  string
-// 	Value     interface{}
-// 	Position  interface{}
-// 	Validated bool
-// }
+type node struct {
+	NodeName  string
+	Value     interface{}
+	Position  interface{}
+	Validated bool
+}
 
-// func extractEntries(config *configuration.Config) map[string]interface{} {
-// 	entries := make(map[string]interface{})
-// 	for _, key := range config.GetKeys() {
-// 		value := config.GetString(key)
-// 		entries[key] = value
-// 	}
-// 	return entries
-// }
+func extractEntries(config *configuration.Config) map[string]interface{} {
+	entries := make(map[string]interface{})
+	for _, key := range config.GetKeys() {
+		value := config.GetString(key)
+		entries[key] = value
+	}
+	return entries
+}
 
-// func createPaths(config *configuration.Config, nodeName string, visited map[string]node) {
-// 	entries := extractEntries(config)
-// 	for key, value := range entries {
-// 		if subConfig, ok := value.(*configuration.Config); ok {
-// 			newNodeName := nodeName + "." + key
-// 			createPaths(subConfig, newNodeName, visited)
-// 		} else {
-// 			currNode, ok := visited[nodeName]
-// 			if !ok {
-// 				currNode = node{}
-// 				currNode.NodeName = nodeName
-// 				visited[nodeName] = currNode
-// 			}
-// 			fmt.Println(nodeName + "." + key)
-// 		}
-// 	}
-// }
+func createPaths(config *configuration.Config, nodeName string, visited map[string]node) {
+	entries := extractEntries(config)
+	for key, value := range entries {
+		if subConfig, ok := value.(*configuration.Config); ok {
+			newNodeName := nodeName + "." + key
+			createPaths(subConfig, newNodeName, visited)
+		} else {
+			currNode, ok := visited[nodeName]
+			if !ok {
+				currNode = node{}
+				currNode.NodeName = nodeName
+				visited[nodeName] = currNode
+			}
+			fmt.Println(nodeName + "." + key)
+		}
+	}
+}
 
-// func main() {
-// 	hoconFile := "/workspaces/confcheck/files/config.conf"
-// 	hoconConf := configuration.LoadConfig(hoconFile)
+func main() {
+	hoconFile := "/workspaces/confcheck/files/config.conf"
+	hoconConf := configuration.LoadConfig(hoconFile)
 
-// 	visited := make(map[string]node)
-// 	createPaths(hoconConf, "", visited)
-// }
+	visited := make(map[string]node)
+	createPaths(hoconConf, "", visited)
+}
